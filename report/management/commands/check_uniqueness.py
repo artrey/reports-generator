29bfa72a237f7d2cfb5c42129aa9221db91601aa
Alexander Ivanov <oz.sasha.ivanov@gmail.com>
2020-10-17 04:38:54 +0300

    Fixed typo

--- report/management/commands/check_uniqueness.py
+++ report/management/commands/check_uniqueness.py
@@ -6,6 +6,6 @@ class Command(BaseCommand):
     help = 'Check and fill uniqueness of reports'
 
     def handle(self, **options):
-        for report_id in models.Report.objects.values_list('id', flat=True)[:2]:
+        for report_id in models.Report.objects.values_list('id', flat=True):
             tasks.check_uniqueness(report_id)
             self.stdout.write(f'Report {report_id} was processed')
