0fe16aea22dfa35b862c703f2f7b83613b817ac1
Alexander Ivanov <oz.sasha.ivanov@gmail.com>
2020-10-19 17:49:04 +0300

    Implemented more accuracy calc ratio

--- report/utils.py
+++ report/utils.py
@@ -35,52 +35,53 @@ def gen_pass(length: int = 14) -> str:
     return password
 
 
-def ratio_reports(src: Report, dst: Report) -> float:
-    global_ratios = []
-    f2_contents = {}
-
-    for f1 in src.source_files.all():  # type: SourceFile
-        f1_content = f1.file.read()
-        ratios = []
-
-        for f2 in dst.source_files.all():  # type: SourceFile
-            f2_content = f2_contents.setdefault(f2.id, f2.file.read())
-            ratio = df.SequenceMatcher(None, f1_content, f2_content).ratio()
-            ratios.append(ratio)
-
-        global_ratios.append(max(ratios))
-
-    return max(global_ratios)
-
-
-def compare_reports(src: Report, dst: Report) -> typing.List[typing.Tuple[float, str]]:
+def match_files_by_ratio(src: Report, dst: Report) -> (list, dict, dict):
     files_names = {}
     files_contents = {}
     ratio_pairs = []
 
     for f1 in src.source_files.all():  # type: SourceFile
         f1_content = files_contents.setdefault(f1.id, f1.file.read())
-        files_names.setdefault(f1.id, basename(f1.file.name))
+        f1_name = files_names.setdefault(f1.id, basename(f1.file.name))
 
         for f2 in dst.source_files.all():  # type: SourceFile
             f2_content = files_contents.setdefault(f2.id, f2.file.read())
-            files_names.setdefault(f2.id, basename(f2.file.name))
+            f2_name = files_names.setdefault(f2.id, basename(f2.file.name))
 
             ratio = df.SequenceMatcher(None, f1_content, f2_content).ratio()
-            ratio_pairs.append((ratio, f1.id, f2.id))
+
+            is_same_names = f1_name == f2_name  # TODO: make with distance
+
+            ratio_pairs.append((is_same_names, ratio, f1.id, f2.id))
+
+            if is_same_names:
+                break
 
     ratio_pairs.sort(reverse=True)
 
     matched_files = []
     while ratio_pairs:
-        ratio, left, right = ratio_pairs.pop(0)
+        _, ratio, left, right = ratio_pairs.pop(0)
         matched_files.append((ratio, left, right))
-        ratio_pairs = list(filter(lambda x: x[1] != left and x[2] != right, ratio_pairs))
+        ratio_pairs = list(filter(lambda x: x[2] != left and x[3] != right, ratio_pairs))
+
+    return matched_files, files_names, files_contents
+
+
+def ratio_reports(src: Report, dst: Report) -> float:
+    matched_files, _, files_contents = match_files_by_ratio(src, dst)
+
+    ratios = sum((ratio * len(files_contents[fid1]) for ratio, fid1, _ in matched_files))
+    return ratios / sum((len(files_contents[fid1]) for _, fid1, _ in matched_files))
+
+
+def compare_reports(src: Report, dst: Report) -> typing.List[typing.Tuple[float, str]]:
+    matched_files, files_names, files_contents = match_files_by_ratio(src, dst)
 
     return [
         (ratio, df.HtmlDiff(tabsize=4).make_file(
-            files_contents[fid1].decode().split(),
-            files_contents[fid2].decode().split(),
+            files_contents[fid1].decode().splitlines(),
+            files_contents[fid2].decode().splitlines(),
             files_names[fid1], files_names[fid2]
         ))
         for ratio, fid1, fid2 in matched_files
